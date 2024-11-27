8ae997e0c2c2353f350ba7b5552db9a7fa370eee
L.Magán <lucasfliper@hotmail.com>
2024-11-27 20:37:31 +0000

    first-commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumber = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let cardSimbol = ["♦", "♥", "♠", "♣"];
+  let ramdonsimbol = getRandom(cardSimbol);
+  let color = "black";
+  if (ramdonsimbol === cardSimbol[0] || ramdonsimbol === cardSimbol[1]) {
+    color = "red";
+  }
+  document.getElementById("topSuit").style.color = color;
+  document.getElementById("botSuit").style.color = color;
+  document.getElementById("numero").innerHTML = getRandom(cardNumber);
+  document.getElementById("topSuit").innerHTML = ramdonsimbol;
+  document.getElementById("botSuit").innerHTML = ramdonsimbol;
 };
+
+setTimeout(function() {
+  location.reload();
+}, 15000);
